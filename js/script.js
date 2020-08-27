6f5802ee10077416697d138052aaf109f6856cdf
ValentinGor <v.gorbatenko@mail.ru>
2020-08-27 21:08:45 +0300

    Изучение примитивных типов данных

--- js/script.js
+++ js/script.js
@@ -1,14 +1,71 @@
-let mobile__menu_open = document.getElementById("mobile__menu_open_btn");
-console.log(mobile__menu_open);
+// let mobile__menu_open = document.getElementById("mobile__menu_open_btn");
+// console.log(mobile__menu_open);
+//
+//
+//
+// document.getElementById("mobile__menu_open_btn").onclick = function () {
+//     document.getElementById("mobile__menu_open_btn").classList.add("active");
+//     document.getElementById("mobile__menu").classList.add("active");
+// }
+//
+// document.getElementById("mobile__menu_closed_btn").onclick = function (){
+//     document.getElementById("mobile__menu_open_btn").classList.remove("active");
+//     document.getElementById("mobile__menu").classList.remove("active");
+// }
+
+// var a;
+// let b;
+
+// console.log(a);
+// console.log(b);
+
+// a = 1;
+// b = 2;
+
+// let a = 1;
+// let b = 9 % 3;
+//
+// console.log(a);
+// console.log(b);
+
+// let a = "1";
+// let b = 2;
+// let c = a + b;
+//
+// let x = 21 / "2";
+//
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(x);
+
+// let a = '1';
+// let a = "1";
 
 
+// let a = true;
+// let a = false;
+// console.log(a);
 
-document.getElementById("mobile__menu_open_btn").onclick = function () {
-    document.getElementById("mobile__menu_open_btn").classList.add("active");
-    document.getElementById("mobile__menu").classList.add("active");
-}
 
-document.getElementById("mobile__menu_closed_btn").onclick = function (){
-    document.getElementById("mobile__menu_open_btn").classList.remove("active");
-    document.getElementById("mobile__menu").classList.remove("active");
-}
+// let a = Infinity;
+// console.log(a);
+
+
+// let a = "Hello";
+// let b = null;
+// let c = a / b;
+//
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+
+let name = "Ilya";
+
+console.log( `hello ${1}` ); // ?
+
+console.log( `hello ${"name"}` ); // ?
+
+console.log( `hello ${name}` ); // ?console.log
